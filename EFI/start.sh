c1e11c64e397e0752d55b85aefa19609c2101714
Danny Chou <50841206+danchouzhou@users.noreply.github.com>
2026-07-12 14:45:49 +0800

    1. Update nftables rules
    2. Remove wol

--- EFI/start.sh
+++ EFI/start.sh
@@ -13,9 +13,6 @@ for i in $(ip link show | grep enp | cut -f2 -d' ' | sed 's/://g'); do
 	echo "auto ${i}" >> /etc/network/interfaces
 	echo "allow-hotplug ${i}" >> /etc/network/interfaces
 	echo "iface ${i} inet dhcp" >> /etc/network/interfaces
-	if ! ethtool ${i} | grep -sq 'Supports Wake-on: d'; then
-		echo "up ethtool -s ${i} wol g" >> /etc/network/interfaces
-	fi
 done
 systemctl restart networking.service
 
@@ -24,5 +21,6 @@ nft add table inet my_table
 nft add chain inet my_table input "{ type filter hook input priority 0 ; policy drop ; }"
 nft add chain inet my_table output "{ type filter hook output priority 0 ; policy accept ; }"
 nft add chain inet my_table forward "{ type filter hook forward priority 0 ; policy drop ; }"
-nft add rule inet my_table input iifname "lo" accept
+nft add rule inet my_table input ct state invalid drop
 nft add rule inet my_table input ct state established,related accept
+nft add rule inet my_table input iifname "lo" accept
